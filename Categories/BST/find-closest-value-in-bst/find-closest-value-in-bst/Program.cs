c9ef7da65e3a667ef99ba1f4a527a363f751a0b2
Lagear <lagear@gmail.com>
2023-03-11 12:12:10 -0400

    solving find-closest-value-in-bst

---
+++ Categories/BST/find-closest-value-in-bst/find-closest-value-in-bst/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace find_closest_value_in_bst;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static int FindClosestValueInBst(BST tree, int target)
+    {
+        int min=0, closet=0;
+        BST current=null;
+
+        if (tree != null)
+        {
+            min = Math.Abs(target - tree.value);
+            closet = tree.value;
+            if (target < tree.value)
+            {
+                current = tree.left;
+            }
+            else
+            {
+                current = tree.right;
+            }
+        }
+
+        bool finished = false;
+
+        while (current != null)
+        {
+            if (min > Math.Abs(target - current.value))
+            {
+                min = Math.Abs(target - current.value);
+                closet = current.value;
+            }
+
+            if (target < current.value)
+            {
+                current = current.left;
+            }
+            else
+            {
+                current = current.right;
+            }
+
+            /*if (current.left != null)
+            {
+                current = current.left;
+            }
+            else if (current.right != null)
+            {
+                current = current.right;
+            }
+            else
+            {
+                current = null;
+            }
+            */
+        }
+
+        // Write your code here.
+        return closet;
+    }
+
+    public class BST
+    {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value)
+        {
+            this.value = value;
+        }
+    }
+}
---
+++ Categories/BST/find-closest-value-in-bst/test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Program = find_closest_value_in_bst.Program;
+
+namespace test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestCase1()
+    {
+        var root = new Program.BST(10);
+        root.left = new Program.BST(5);
+        root.left.left = new Program.BST(2);
+        root.left.left.left = new Program.BST(1);
+        root.left.right = new Program.BST(5);
+        root.right = new Program.BST(15);
+        root.right.left = new Program.BST(13);
+        root.right.left.right = new Program.BST(14);
+        root.right.right = new Program.BST(22);
+
+        var expected = 13;
+        var actual = Program.FindClosestValueInBst(root, 12);
+        Assert.AreEqual(expected, actual);
+    }
+}
