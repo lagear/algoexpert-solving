b655e1e80f196d1a155a1bb30cd8742a2cb92b19
Romero <lagear@gmail.com>
2023-05-15 09:25:51 -0400

    More solutions.

---
+++ Categories/Arrays/MoveElementToEnd/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoveElementToEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var array = new List<int> { 2, 1, 2, 2, 2, 3, 4, 2 };
+            var toMove = 2;
+            var result = MoveElementToEnd(array, toMove);*/
+            var array = new List<int> {3, 1, 2, 4, 5};
+            var toMove = 3;
+            var result = MoveElementToEnd(array, toMove);
+        }
+
+        public static List<int> MoveElementToEnd(List<int> array, int toMove)
+        {
+            // Write your code here.
+            int lastIndex = array.Count - 1;
+            int lastIndexToMove = lastIndex -1;
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (array[i] == toMove)
+                {
+
+                    while (array[lastIndex] == toMove)
+                    {
+                        lastIndexToMove = lastIndex;
+                        lastIndex--;
+                        if (lastIndex < 0)
+                        {
+                            return array;
+                        }
+                    }
+
+                    if (lastIndexToMove > i)
+                    {
+                        int tmp = array[lastIndex];
+                        array[lastIndex] = array[i];
+                        array[i] = tmp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ Categories/Arrays/SmallestDifference/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http.Headers;
+
+namespace SmallestDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne = new int[] { -1, 5, 10, 20, 28, 3 };
+            int[] arrayTwo = new int[] { 26, 134, 135, 15, 17 };
+            var result = SmallestDifference(arrayOne, arrayTwo);
+        }
+
+        public static int[] SmallestDifference(int[] arrayOne, int[] arrayTwo)
+        {
+            // Write your code here.
+            int min = 0;
+            int minOne = arrayOne[0];
+            int minTwo = arrayTwo[0];
+            List<Item> aMinusb = new List<Item>();
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                for (int j = 0; j < arrayTwo.Length; j++)
+                {
+                    aMinusb.Add(
+                        new Item()
+                        {
+                            Difference = Math.Abs(arrayOne[i] - arrayTwo[j]),
+                            OneValue = arrayOne[i],
+                            TwoValue = arrayTwo[j]
+                        }
+                        );
+                }
+            }
+
+            var orderred = aMinusb.OrderBy(x => x.Difference).ToList().FirstOrDefault();
+            if (orderred != null)
+            {
+                return new int[] { orderred.OneValue, orderred.TwoValue };
+            } else 
+            {
+                return new int[] { };
+            };
+        }
+
+        class Item { 
+            public int Difference { get; set; }
+            public int OneValue { get; set; }   
+            public int TwoValue { get; set; }
+        }
+    }
+}
